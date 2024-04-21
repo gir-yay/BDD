8f5c0d4a2d30eb6ec4d20d287908bf3000f9338a
ghizlane <ghlaneiz022@gmail.com>
2024-04-21 20:28:35 +0100

    manager

--- DB_project/DB_project/urls.py
+++ DB_project/DB_project/urls.py
@@ -15,28 +15,28 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from appCar.views import home, ajouter_voiture,dashboard,our_managers,modifier_manager,supprimer_manager,ajouter_manager,dashboard_manager,login_view
+from appCar.views import home, ajouter_voiture,dashboard,our_managers,our_admins,modifier_manager,supprimer_manager,ajouter_manager,dashboard_manager,login_view,logout_view,our_clients,our_cars,our_reservations
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('', home, name='home'),
-      path('ajouter_voiture/', ajouter_voiture, name='ajouter_voiture'), 
-      
-      
-
-
-
-       path('login/', login_view, name='login_view'),
-        path('admin_dashboard/', dashboard, name='admin_dashboard'),
-        path('our-managers/', our_managers, name='our_managers'),
-         # URL pour la modification d'un gestionnaire
-    path('modifier-manager/<int:manager_id>/', modifier_manager, name='modifier_manager'),
-
-    # URL pour la suppression d'un gestionnaire
-     path('supprimer-manager/<int:manager_id>/', supprimer_manager, name='supprimer_manager'),
-    path('ajouter-manager/', ajouter_manager, name='ajouter_manager'),
+    path('', home, name='home'),
+    path('ajouter_voiture/', ajouter_voiture, name='ajouter_voiture'), 
+    path('login/', login_view , name='login_view'),
+    path('logout/', logout_view, name='logout'),
+    path('admin_dashboard/', dashboard, name='admin_dashboard'),
     path('dashboard_manager/', dashboard_manager, name='dashboard_manager'),
+    path('our-managers/', our_managers, name='our_managers'),
+    path('our-admins/', our_admins, name='our_admins'),
+    path('our-clients/', our_clients, name='our_clients'),
+    path('our-cars/', our_cars, name='our_cars'),
+    path('our-reservations/', our_reservations, name='our_reservations'),
+    # URL pour la modification d'un gestionnaire
+    path('modifier-manager/<int:manager_id>/', modifier_manager, name='modifier_manager'),
+    # URL pour la suppression d'un gestionnaire
+    path('supprimer-manager/<int:manager_id>/', supprimer_manager, name='supprimer_manager'),
+    path('ajouter-manager/', ajouter_manager, name='ajouter_manager'),
+   
     
 
 ]
