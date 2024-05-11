57b0d04b31d4728459a084e7e0c62bdf2052d519
ghizlane <ghlaneiz022@gmail.com>
2024-05-11 19:22:15 +0100

    virtual env

--- DB_project/DB_project/settings.py
+++ DB_project/DB_project/settings.py
@@ -74,18 +74,21 @@ WSGI_APPLICATION = 'DB_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+'''
 
 DATABASES = {
     'default': {
         'ENGINE': 'djongo',
         'NAME':'locationv_db',
+        'HOST': 'localhost',
+        'PORT': 27017,
     }
 }
 
